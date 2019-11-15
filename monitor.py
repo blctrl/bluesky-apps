5c930db7c727ecb65f71ee92f2588323171b319e
blctrl <zhangzhaohong@sinap.ac.cn>
2019-11-15 15:14:09 +0800

    add install_qt_kicker to be compatible in windows

--- monitor.py
+++ monitor.py
@@ -5,5 +5,8 @@ from bluesky.plans import count, scan
 from bluesky.callbacks import LivePlot
 
 from ophyd.sim import noisy_det
+from bluesky.utils import install_qt_kicker
+install_qt_kicker()
 
-RE(count([noisy_det], num=1000, delay = 0.1), LivePlot('noisy_det'))
+
+RE(count([noisy_det], num=200, delay = 0.1), LivePlot('noisy_det'))
