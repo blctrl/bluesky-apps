10ec39521296289cad8d7e8d7a0ad5bea6ed9d0a
blctrl <zhangzhaohong@sinap.ac.cn>
2019-11-18 14:34:51 +0800

    monitor for win10 and linux

---
+++ monitor_linux.py
@@ -0,0 +1,11 @@
+from bluesky import RunEngine
+RE = RunEngine({})
+
+from bluesky.plans import count, scan
+from bluesky.callbacks import LivePlot
+
+from ophyd.sim import noisy_det
+
+
+
+RE(count([noisy_det], num=200, delay = 0.1), LivePlot('noisy_det'))
---
+++ monitor_win10.py
@@ -0,0 +1,20 @@
+#monitor_win10
+
+from bluesky import RunEngine
+RE = RunEngine({})
+
+from bluesky import RunEngine
+RE = RunEngine({})
+
+from bluesky.plans import count, scan
+from bluesky.callbacks import LivePlot
+
+from ophyd.sim import noisy_det
+
+from bluesky.utils import install_kicker
+install_kicker()
+
+RE(count([noisy_det], num=50, delay = 0.1), LivePlot('noisy_det'))
+
+
+
